abb2beb2bc1039a4d8e6801a639eb2719e6a147f
bansi <bansidobariya2510@gmail.com>
2025-10-02 14:32:58 +0100

    tool stack add

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -11,32 +11,17 @@ function AboutCard() {
             Hi Everyone , <br /> <br /> I am{" "}
             <span className="purple">Bansi Dobariya, </span>
             <span className="purple"></span>
-            I was employed as a software developer at PAR Solution.
+            I am currently working as a Full stack devloper at Newcastle
+            University and previously worked at Par Solotuion as a Software
+            developer.
             <br />
-            I am currently pursuing an MSc in Advanced Computer Science at
-            Newcastle University, UK, and I hold a bachelor's degree in
-            Information Technology (IT) .
+            I have got Master degree in Advanced Computer Science at Newcastle
+            University, UK, and I hold a bachelor's degree in Information
+            Technology (IT) .
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+            Apart from coding, I do not visualize my life :)
           </p>
-          <ul>
-            <li className="about-activity">
-              <ImPointRight />
-              Playing Cricket
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Board Game Design
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Urban Exploring
-            </li>
-          </ul>
-
-          <p style={{ color: "rgb(155 126 172)" }}>
-            "Hard work and strategy always outplay raw talent."{" "}
-          </p>
-          <footer className="blockquote-footer">Bansi</footer>
         </blockquote>
       </Card.Body>
     </Card>
